52723fc28b3103a47cb37fdb00e31d84a60baee2
vedantvyas9 <96966153+vedantvyas9@users.noreply.github.com>
2026-06-01 14:28:17 -0500

    Add: foreign-dictionary - submission-1

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-1.py
@@ -0,0 +1,61 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        graph = defaultdict(set)
+        indegree = {}
+        for word in words:
+            for c in word:
+                graph[c] = set()
+                indegree[c] = 0
+        def compare(word1, word2):
+            for i in range(min(len(word1), len(word2))):
+                if (word1[i] != word2[i]):
+                    if (word2[i] not in graph[word1[i]]):
+                        graph[word1[i]].add(word2[i])
+                        indegree[word2[i]] += 1
+                    return True
+            if (len(word1) > len(word2)):
+                return False
+            return True
+        for i in range(len(words) - 1):
+            if (not compare(words[i], words[i + 1])):
+                return ""
+        visited = set()
+        stack = set()
+        output = []
+        # def dfs(curr):
+        #     if (curr in stack):
+        #         return False
+        #     if (curr in visited):
+        #         return True
+        #     stack.add(curr)
+        #     for neigh in graph[curr]:
+                
+        #         if not dfs(neigh):
+        #             return False
+        #     visited.add(curr)
+        #     stack.remove(curr)
+        #     output.append(curr)
+        #     return True
+        que = deque([])
+        for key, val in indegree.items():
+            if (val == 0):
+                que.append(key)
+        while (que):
+            tot_len = len(que)
+            for i in range(tot_len):
+                curr = que.popleft()
+                output.append(curr)
+                for neigh in graph[curr]:
+                    indegree[neigh] -= 1
+                    if (indegree[neigh] == 0):
+                        que.append(neigh)
+        if (len(output) != len(graph)):
+            return ""
+        return "".join(output)
+        # for c in graph:
+        #     if c not in visited:
+        #         if not dfs(c):
+        #             return ""
+        # return "".join(output[::-1])
+
+            
